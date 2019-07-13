23de87b3e366e730270b48fb22aee55c6d1304e2
Prince V G <princegracys@gmail.com>
2019-07-14 02:48:00 +0530

    updated guide.json and fetching the steps from the json file

--- player.js
+++ player.js
@@ -1,25 +1,14 @@
-var tour = new Tour({
-    steps: [{
-        "id": "1",
-        "content": "tip on first div",
-        "element": "#id_1",
-        "next": "1",
-        "placement": "bottom"
-    },{
-        "id": "2",
-        "content": "tip on second div",
-        "element": ".myClass2",
-        "next": "2",
-        "placement": "left"
-    },{
-        "id": "3",
-        "content": "tip on third div.",
-        "element": "div:eq(2)",
-        "next": "0",
-        "placement": "right"
-    }],
-    backdrop: true,
-    backdropContainer: 'body',
-    backdropPadding: 0,
+$(document).ready(function(){   
+    var tour
+    $.getJSON("guide.json", function(result){
+        tour = new Tour($.extend({}, {
+            backdrop: true,
+            backdropContainer: 'body',
+            backdropPadding: 0,
+        }, result));
+      });
+    $(".explore").on("click", function(){
+       tour.start(true);
+    })
 });
 
