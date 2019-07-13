90e569e61b5beb8d35d73ff9f97520a9f246cad4
Prince V G <princegracys@gmail.com>
2019-07-14 02:18:10 +0530

    Added bootstrap-tour library

--- player.js
+++ player.js
@@ -3,24 +3,23 @@ var tour = new Tour({
         "id": "1",
         "content": "tip on first div",
         "element": "#id_1",
-        "next": "1"
+        "next": "1",
+        "placement": "bottom"
     },{
         "id": "2",
         "content": "tip on second div",
         "element": ".myClass2",
-        "next": "2"
+        "next": "2",
+        "placement": "left"
     },{
         "id": "3",
         "content": "tip on third div.",
         "element": "div:eq(2)",
-        "next": "0"
+        "next": "0",
+        "placement": "right"
     }],
     backdrop: true,
     backdropContainer: 'body',
-    backdropPadding: 0,});
-  
-  // Initialize the tour
-  tour.init();
-  
-  // Start the tour
-  tour.start();
+    backdropPadding: 0,
+});
+
